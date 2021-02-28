07f07f420ae9b28f5b38a2c18f8e05b579860d2c
Alex Smith <fnkcollective@hotmail.co.uk>
2021-02-28 09:42:42 +0000

    Fixes jest for mjs files

--- server/src/hello.test.js
+++ server/src/hello.test.js
@@ -1,5 +1,10 @@
+import request from "superagent";
+import { app } from "./app.mjs";
+
 describe("", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
+  it("should pass", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.statusCode).toBe(200);
   });
 });
