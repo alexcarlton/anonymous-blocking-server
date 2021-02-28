eeb3cff0fa5bdd72a0cd0f4f0bd53d4b0d5083c9
Alex Smith <fnkcollective@hotmail.co.uk>
2021-02-28 12:05:34 +0000

    Adds session on start session

--- server/src/handlers/sessions/startSession.mjs
+++ server/src/handlers/sessions/startSession.mjs
@@ -1,4 +1,10 @@
+import { addSession } from "../../database/queries/sessions/addSession.mjs";
+
 function startSession(req, res) {
+  const session = req.body;
+
+  addSession({ userId: req.user.id, session });
+
   return res.send();
 }
 
