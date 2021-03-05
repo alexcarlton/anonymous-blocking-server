1db29d20d0414741c1c2401b1c63de5738517e9b
Alex Smith <fnkcollective@hotmail.co.uk>
2021-03-05 16:35:23 +0000

    ADds logic to start and stop one-off schedules

--- server/src/database/queries/schedules/addSchedule.mjs
+++ server/src/database/queries/schedules/addSchedule.mjs
@@ -1,11 +1,44 @@
+import scheduler from "node-schedule";
+import { DateTime } from "luxon";
 import { data } from "../../data.mjs";
+import { setScheduleIsActive } from "./setScheduleIsActive.mjs";
+
+const startSchedule = ({ scheduleId, userId, startDate, duration }) => {
+  console.log(`Activated schedule ${scheduleId} for user ${userId}`);
+
+  setScheduleIsActive({ scheduleId, userId, isActive: true });
+
+  const endDate = DateTime.fromISO(startDate)
+    .plus({ minutes: duration })
+    .toMillis();
+
+  scheduler.scheduleJob(endDate, () => {
+    console.log(`De-activated schedule ${scheduleId} for user ${userId}`);
+
+    setScheduleIsActive({ userId, scheduleId, isActive: false });
+  });
+};
 
 function addSchedule({ userId, schedule }) {
   const existingSchedules = data.schedules.byUserId[userId];
+
   data.schedules.byUserId[userId] = {
     ...existingSchedules,
-    [schedule.id]: schedule,
+    [schedule.id]: { ...schedule, isActive: false },
   };
+
+  if (schedule.type === "one-off") {
+    const startDate = DateTime.fromISO(schedule.startDate).toMillis();
+
+    return scheduler.scheduleJob(startDate, () =>
+      startSchedule({
+        scheduleId: schedule.id,
+        userId,
+        startDate: schedule.startDate,
+        duration: schedule.duration,
+      })
+    );
+  }
 }
 
 export { addSchedule };
