2aa15f5d7042ae156f3be63a6ae12712fa6dbb66
Alex Smith <fnkcollective@hotmail.co.uk>
2021-03-05 15:11:08 +0000

    Cancels existing endSessionJob on new session sent

--- server/src/database/queries/sessions/addSession.mjs
+++ server/src/database/queries/sessions/addSession.mjs
@@ -1,17 +1,33 @@
 import { data } from "../../data.mjs";
 import schedule from "node-schedule";
 
-function addSession({ userId, session }) {
-  if (session.endDate) {
-    const parsedDate = Date.parse(session.endDate)
-    const date = new Date(parsedDate)
+const cancelCurrentSession = (userId) => {
+  const existingSession = data.sessions.byUserId[userId];
 
-    const job = schedule.scheduleJob(date, () => {
+  existingSession?.endSessionJob?.cancel();
+
+  data.sessions.byUserId[userId] = null;
+};
+
+function addSession({ userId, session }) {
+  cancelCurrentSession(userId);
+
+  let endSessionJob;
+
+  if (session.endDate) {
+    const parsedDate = Date.parse(session.endDate);
+    const date = new Date(parsedDate);
+
+    endSessionJob = schedule.scheduleJob(date, () => {
+      console.log(`Ended session for user ${userId}`);
+
       data.sessions.byUserId[userId] = null;
-    })
+    });
   }
 
-  data.sessions.byUserId[userId] = session;
+  data.sessions.byUserId[userId] = session.endDate
+    ? { ...session, endSessionJob }
+    : session;
 }
 
 export { addSession };
