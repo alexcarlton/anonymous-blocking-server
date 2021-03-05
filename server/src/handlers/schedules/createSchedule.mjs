deffa2e246f21b32c4061d420461cadad4e47461
Alex Smith <fnkcollective@hotmail.co.uk>
2021-03-05 16:44:52 +0000

    Adds check for schedule with sent ID in createSchedule

--- server/src/handlers/schedules/createSchedule.mjs
+++ server/src/handlers/schedules/createSchedule.mjs
@@ -1,9 +1,17 @@
 import { addSchedule } from "../../database/queries/schedules/addSchedule.mjs";
+import { getSchedule } from "../../database/queries/schedules/getSchedule.mjs";
 
 function createSchedule(req, res) {
-  const schedule = { ...req.body, id: req.params.scheduleId };
+  const userId = req.user.id;
+  const id = req.params.scheduleId;
 
-  addSchedule({ userId: req.user.id, schedule });
+  const schedule = { ...req.body, id };
+
+  if (getSchedule({ userId, scheduleId: id })) {
+    return res.status(409).send();
+  }
+
+  addSchedule({ userId, schedule });
 
   res.send();
 }
--- server/src/handlers/tests/schedules/createSchedule.test.js
+++ server/src/handlers/tests/schedules/createSchedule.test.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import request from "supertest";
 import { app } from "../../../app.mjs";
 import { data } from "../../../database/data.mjs";
+import { addSchedule } from "../../../database/queries/schedules/addSchedule.mjs";
 
 describe("createSchedule", () => {
   const userId = "123";
@@ -52,6 +53,30 @@ describe("createSchedule", () => {
     expect(response.statusCode).toBe(400);
   });
 
-  it("should setup the schedule start and end dates (repeated)", () => {});
-  it("should setup the schedule start and end dates (one-off)", () => {});
+  it("should return a 409 if a schedule already exists with the sent id", async () => {
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET);
+
+    const schedule = {
+      name: "Morning Focus",
+      type: "repeated",
+      services: [{ name: "facebook" }, { name: "reddit" }],
+      duration: 120,
+      repeats: ["* * 14 * * 1", "* * 14 * * 3"],
+    };
+
+    const scheduleId = "1";
+
+    addSchedule({ userId, schedule: { ...schedule, id: scheduleId } });
+
+    const response = await request(app)
+      .post(`/schedules/${scheduleId}`)
+      .send(schedule)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(409);
+
+    expect(data.schedules.byUserId[userId]).toEqual({
+      [scheduleId]: { ...schedule, id: scheduleId, isActive: false },
+    });
+  });
 });
