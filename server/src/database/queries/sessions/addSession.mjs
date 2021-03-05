8eea260821d5ec804fcda762cc5215d13c3f49dc
Alex Smith <fnkcollective@hotmail.co.uk>
2021-03-05 14:58:46 +0000

    Updates addSession to schedule cancelling the session

--- server/src/database/queries/sessions/addSession.mjs
+++ server/src/database/queries/sessions/addSession.mjs
@@ -1,6 +1,16 @@
 import { data } from "../../data.mjs";
+import schedule from "node-schedule";
 
 function addSession({ userId, session }) {
+  if (session.endDate) {
+    const parsedDate = Date.parse(session.endDate)
+    const date = new Date(parsedDate)
+
+    const job = schedule.scheduleJob(date, () => {
+      data.sessions.byUserId[userId] = null;
+    })
+  }
+
   data.sessions.byUserId[userId] = session;
 }
 
