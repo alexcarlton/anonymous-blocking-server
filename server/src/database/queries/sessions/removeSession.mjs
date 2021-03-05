051a329183875eaa0e47888f7b40929cc586cd0a
Alex Smith <fnkcollective@hotmail.co.uk>
2021-03-05 15:32:22 +0000

    Cancels current session endSessionJob on stopSession request

--- server/src/database/queries/sessions/removeSession.mjs
+++ server/src/database/queries/sessions/removeSession.mjs
@@ -1,6 +1,8 @@
 import { data } from "../../data.mjs";
 
 function removeSession({ userId }) {
+  data.sessions.byUserId[userId]?.endSessionJob?.cancel();
+
   data.sessions.byUserId[userId] = null;
 }
 
