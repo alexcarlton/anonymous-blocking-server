733ea16c6d081c29d6e1b3572157d075c4ea660a
Alex Smith <fnkcollective@hotmail.co.uk>
2021-03-05 15:38:27 +0000

    Adds empty tests

---
+++ server/src/handlers/tests/schedules/createSchedule.test.js
@@ -0,0 +1,7 @@
+describe("createSchedule", () => {
+  it("should add a schedule to the database", () => {});
+
+  it("should return a 400 if the request is not valid", () => {});
+
+  it("should setup the schedule start and end dates", () => {});
+});
