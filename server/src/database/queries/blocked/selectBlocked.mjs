9e5b0cd75118c6a28069ecb4ae0ada71d494e929
Alex Smith <fnkcollective@hotmail.co.uk>
2021-03-06 08:42:19 +0000

    Passes  session running test

---
+++ server/src/database/queries/blocked/selectBlocked.mjs
@@ -0,0 +1,35 @@
+import { getSession } from "../sessions/getSession.mjs";
+import { selectSchedules } from "../schedules/selectSchedules.mjs";
+
+const getRunningSchedule = ({ userId }) => {
+  const schedules = selectSchedules({ userId });
+
+  return Object.values(schedules).find((schedule) => schedule.isActive);
+};
+
+function selectBlocked({ userId }) {
+  const runningSession = getSession({ userId });
+  const runningSchedule = getRunningSchedule({ userId });
+
+  if (!runningSchedule && !runningSession) {
+    return {
+      data: {
+        blocker: null,
+        services: [],
+      },
+    };
+  }
+
+  if (runningSession) {
+    return {
+      data: {
+        blocker: {
+          type: "session",
+        },
+        services: runningSession.services,
+      },
+    };
+  }
+}
+
+export { selectBlocked };
--- server/src/handlers/blocked/getBlocked.mjs
+++ server/src/handlers/blocked/getBlocked.mjs
@@ -1,28 +1,11 @@
-import { getSession } from "../../database/queries/sessions/getSession.mjs";
-import { selectSchedules } from "../../database/queries/schedules/selectSchedules.mjs";
-
-const getRunningSchedule = ({ userId }) => {
-  const schedules = selectSchedules({ userId });
-
-  return Object.values(schedules).find((schedule) => schedule.isActive);
-};
+import { selectBlocked } from "../../database/queries/blocked/selectBlocked.mjs";
 
 function getBlocked(req, res) {
   const userId = req.user.id;
 
-  const runningSession = getSession({ userId });
-  const runningSchedule = getRunningSchedule({ userId });
+  const blocked = selectBlocked({ userId });
 
-  if (!runningSchedule && !runningSession) {
-    return res.json({
-      data: {
-        blocker: null,
-        services: [],
-      },
-    });
-  }
-
-  res.send();
+  res.json(blocked);
 }
 
 export { getBlocked };
